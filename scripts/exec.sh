e2708d9b90d831249bbd47622247c62a99a92b5a
Jannik Obermann <jannik.obermann@gmail.com>
2024-11-04 22:37:46 +0100

    Update exec.sh

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-chmod +x scripts/setup.sh
-chmod +x scripts/start.sh
-chmod +x scripts/stop.sh
-chmod +x scripts/test.sh
+chmod +x setup.sh
+chmod +x start.sh
+chmod +x stop.sh
+chmod +x test.sh
