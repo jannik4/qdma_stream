2e25843ee73e517212c105856b1d0b8b619761ed
Jannik Obermann <jannik.obermann@gmail.com>
2024-12-12 01:25:06 +0100

    Update scripts

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# Set default count to 4 if not provided
-count=${1:-4}
+# Set default count_h2c to 4 if not provided
+count_h2c=${1:-4}
+
+# Set default count_c2h to count_h2c if not provided
+count_c2h=${2:-$count_h2c}
 
 echo "### Stopping ${count} queues ###"
 
 # H2C (Host to Card)
-for (( i=0; i<count; i++ )); do
+for (( i=0; i<count_h2c; i++ )); do
     dma-ctl qdmac1000 q stop idx $i dir h2c
     dma-ctl qdmac1000 q del idx $i dir h2c
 done
 
 # C2H (Card to Host)
-for (( i=0; i<count; i++ )); do
+for (( i=0; i<count_c2h; i++ )); do
     dma-ctl qdmac1000 q stop idx $i dir c2h
     dma-ctl qdmac1000 q del idx $i dir c2h
 done
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Set default count to 4 if not provided
-count=${1:-4}
+# Set default count_h2c to 4 if not provided
+count_h2c=${1:-4}
+
+# Set default count_c2h to count_h2c if not provided
+count_c2h=${2:-$count_h2c}
 
 cd "$(dirname "$0")"
 
 # Start queues
-./start.sh $count
+./start.sh $count_h2c $count_c2h
 echo
 
 # H2C
 echo "### Run H2C ###"
-for (( i=0; i<count; i++ )); do
+for (( i=0; i<count_h2c; i++ )); do
     dma-to-device -d /dev/qdmac1000-ST-$i -s 4096 -c 262144 &
 done
 wait
@@ -19,16 +22,16 @@ echo
 
 # H2C Rust
 echo "### Run H2C Rust ###"
-../target/release/examples/simple $count
+../target/release/examples/simple $count_h2c
 echo
 
 # C2H
 echo "### Run C2H ###"
-for (( i=0; i<count; i++ )); do
+for (( i=0; i<count_c2h; i++ )); do
     dma-from-device -d /dev/qdmac1000-ST-$i -s 4096 -c 262144 &
 done
 wait
 echo
 
 # Stop queues
-./stop.sh $count
+./stop.sh $count_h2c $count_c2h
