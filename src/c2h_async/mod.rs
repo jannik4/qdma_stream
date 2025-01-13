eafb0ba6047deb35098d6d91c9b42ceae25db1e9
Jannik Obermann <jannik.obermann@gmail.com>
2025-01-13 17:23:29 +0100

    async improvements

--- src/c2h_async/mod.rs
+++ src/c2h_async/mod.rs
@@ -7,10 +7,11 @@ use monoio::fs;
 pub struct CardToHostStreamAsync {
     file: fs::File,
     buf: Option<Buf>,
+    pos: u64,
 }
 
 impl CardToHostStreamAsync {
-    pub const PACKET_SIZE: usize = 4096;
+    pub const PACKET_SIZE: usize = 4_096_000;
     const ALIGN: usize = 4096;
 
     pub async fn new(path: impl AsRef<std::path::Path>) -> Result<Self> {
@@ -21,10 +22,15 @@ impl CardToHostStreamAsync {
 
         let buf = Some(Buf::new()?);
 
-        Ok(Self { file, buf })
+        Ok(Self { file, buf, pos: 0 })
     }
 
-    pub async fn next_packet(&mut self) -> Result<Option<&[u8]>> {
+    pub async fn next_packet(&mut self) -> Result<&[u8]> {
+        self.read().await?;
+        Ok(self.slice())
+    }
+
+    pub async fn next_packet_or_ctrl_seq(&mut self) -> Result<Option<&[u8]>> {
         self.read().await?;
         if self.slice().starts_with(&CTRL_SEQ) {
             self.read().await?;
@@ -40,9 +46,11 @@ impl CardToHostStreamAsync {
 
     async fn read(&mut self) -> Result<()> {
         let buf = self.buf.take().unwrap();
-        let (res, buf) = self.file.read_exact_at(buf, 0).await;
+        let (res, buf) = self.file.read_exact_at(buf, self.pos).await;
         self.buf = Some(buf);
-        Ok(res?)
+        res?;
+        self.pos += Self::PACKET_SIZE as u64;
+        Ok(())
     }
 
     fn slice(&self) -> &[u8] {
