32b51c59d86a02527cc1ae493be37b8400950a5b
Jannik Obermann <jannik.obermann@gmail.com>
2025-03-24 11:33:22 +0100

    Remove async variants

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 mod c2h;
-mod c2h_async;
 mod h2c;
 mod util;
 
 pub mod ctl;
 
-pub use self::{c2h::CardToHostStream, c2h_async::CardToHostStreamAsync, h2c::HostToCardStream};
+pub use self::{c2h::CardToHostStream, h2c::HostToCardStream};
